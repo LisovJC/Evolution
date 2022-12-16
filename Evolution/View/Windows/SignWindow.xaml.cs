baec736f305403818c0fc90058534fda9df40d0a
Ivan <93536115+LisovJC@users.noreply.github.com>
2022-12-16 22:21:07 +0300

    feat: SignWindow updated

--- Evolution/View/Windows/SignWindow.xaml.cs
+++ Evolution/View/Windows/SignWindow.xaml.cs
@@ -22,6 +22,20 @@ namespace Evolution.View.Windows
         public SignWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                {
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                }
+                this.DragMove();
+            }
+
         }
     }
 }
